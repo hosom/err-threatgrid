066408ee7b48ad7999e4a26b3fce61aa1cb9c288
Zack Nagaich <zacknagaich@gmail.com>
2016-05-24 14:25:40 -0400

    Refactoring and comments

--- threatgrid.py
+++ threatgrid.py
@@ -10,8 +10,42 @@ class ThreatGrid(BotPlugin):
             'search_width' : '30 days ago'
         }
 
-    def get_sample_info(self,params):
-        
+    '''
+       Given a set of samples, enrich each sample ID with related fields from sample info record.        
+
+       Params:
+           sample_ids - Set of samples we will enumerate through and enrich
+ 
+       Returns:
+           reply - String containing Slack markup for displaying out sample information
+    ''' 
+    def enrich_samples(self,sample_ids):
+        reply = ''
+        if len(sample_ids) < 15: 
+            for sample in sample_ids:
+                reply += "Sample \*%s\*\n" % (sample)
+                sample_dict = self.get_sample_info({'api_key':self.config['api_key'],'after':self.config['search_width'],'id':sample})
+                reply += '\t\*FileName\*: %s\n\t\*OS\*: %s\n\t\*SHA1\*: %s\n\t\*MD5\*: %s\n\t\*SHA256\*: %s\n' % (sample_dict['filename'],sample_dict['os'],sample_dict['sha1'],sample_dict['md5'],sample_dict['sha256'])
+                reply += '\t\*ThreatGrid Link\*: `https://panacea.threatgrid.com/samples/%s`\n' % (sample)
+               
+ 
+        else:
+            for sample in sample_ids:
+                reply += "Sample \*%s\*\n" % (sample)
+
+        return reply
+
+    
+    '''
+       Returns dictionary representing sample info record from threatgrid.        
+
+       Params:
+           params - paramters needed for threatgrid API call
+ 
+       Returns:
+           reply - Dictionary representing sample info record
+    '''
+    def get_sample_info(self,params):        
         try:
             reply = {}
             for results_group in threatgrid.samples(params):
@@ -26,6 +60,7 @@ class ThreatGrid(BotPlugin):
         except:
             return None
 
+                
     @botcmd(admin_only=True)
     def tg_hashlookup(self, msg, args):
         '''Lookup a hash using ThreatGrid's API.'''
@@ -43,7 +78,6 @@ class ThreatGrid(BotPlugin):
 
         success = False
         sample_ids = set()
-        reply = ''
             
         yield ("Searching ThreatGrid API...\n")
         
@@ -55,18 +89,8 @@ class ThreatGrid(BotPlugin):
                     for result in results_group[u'data'][u'items']:
                         sample_ids.add(result[u'sample'])
                 
-                    if len(sample_ids) < 15: 
-                        for sample in sample_ids:
-                            reply += "Sample \*%s\*\n" % (sample)
-                            sample_dict = self.get_sample_info({'api_key':self.config['api_key'],'after':self.config['search_width'],'id':result[u'sample']})
-                            reply += '\t\*FileName\*: %s\n\t\*OS\*: %s\n\t\*SHA1\*: %s\n\t\*MD5\*: %s\n\t\*SHA256\*: %s\n' % (sample_dict['filename'],sample_dict['os'],sample_dict['sha1'],sample_dict['md5'],sample_dict['sha256'])
-                            reply += '\t\*ThreatGrid Link\*: `https://panacea.threatgrid.com/samples/%s`\n' % (sample)
-                            yield reply
- 
-                    else:
-                        for sample in sample_ids:
-                            reply += "Sample \*%s\*\n" % (sample)
-                      
+                    yield self.enrich_samples(sample_ids)
+       
             if not success:
                 yield 'No matches found.'
        
@@ -92,7 +116,6 @@ class ThreatGrid(BotPlugin):
 
         success = False
         sample_ids = set()
-        reply = ''
         
         yield ("Searching ThreatGrid API...\n")
 
@@ -104,18 +127,8 @@ class ThreatGrid(BotPlugin):
                     for result in results_group[u'data'][u'items']:
                         sample_ids.add(result[u'sample'])
                 
-                    if len(sample_ids) < 15: 
-                        for sample in sample_ids:
-                            reply += "Sample \*%s\*\n" % (sample)
-                            sample_dict = self.get_sample_info({'api_key':self.config['api_key'],'after':self.config['search_width'],'id':result[u'sample']})
-                            reply += '\t\*FileName\*: %s\n\t\*OS\*: %s\n\t\*SHA1\*: %s\n\t\*MD5\*: %s\n\t\*SHA256\*: %s\n' % (sample_dict['filename'],sample_dict['os'],sample_dict['sha1'],sample_dict['md5'],sample_dict['sha256'])
-                            reply += '\t\*ThreatGrid Link\*: `https://panacea.threatgrid.com/samples/%s`\n' % (sample)
-                            yield reply
- 
-                    else:
-                        for sample in sample_ids:
-                            reply += "Sample \*%s\*\n" % (sample)
-                      
+                    yield self.enrich_samples(sample_ids)
+       
             if not success:
                 yield 'No matches found.'
        
